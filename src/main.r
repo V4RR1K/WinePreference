82346143f3c9e1b455c15aaf99267a19e957da42
Greg Lynskey <gclynskey@outlook.com>
2023-01-27 09:56:26 -0500

    Reading CSV

--- src/main.r
+++ src/main.r
@@ -1,3 +1,11 @@
 # Greg Lynskey
 
-print(sqrt(2))
+rm(list = ls())
+setwd("~/Desktop/Ordon/R/WinePreference/WinePreference")
+wine_data <- read.csv(file = "wine-preference.csv", header = TRUE)
+library(pwt10)
+library(dplyr)
+
+data(pwt10.0)
+
+attach(pwt10.0)
